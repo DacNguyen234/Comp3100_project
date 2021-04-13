4e7e7e52c0306a2510dfd63fd27c9f4df0bf248f
cuongnguyen44491158 <54030068+cuongnguyen44491158@users.noreply.github.com>
2021-04-13 12:51:44 +0700

    Add files via upload
    Fix bugs, generates logs exactly like the ones shown in user guide

--- MyClient.java
+++ MyClient.java
@@ -1,21 +1,14 @@
 import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.io.*;
-import java.util.ArrayList;
-import java.lang.Integer;
-
 
 public class MyClient {
     public static String HELO = "HELO";
-    public static String AUTH = "AUTH user";
+    public static String AUTH = "AUTH group20";
     public static String REDY = "REDY";
     public static String QUIT = "QUIT";
-    public static char[] HI = {'H', 'E','L', 'O'};
-    public int coreCount = -1;
 
-    public MyClient(){
-
-    }
+    public MyClient(){   }
 
     public String readMsg(byte[] b, BufferedInputStream bis) {
         try {
@@ -27,6 +20,7 @@ public class MyClient {
         }
         return "error";
     }
+
     public static void main (String args[]) throws Exception {
         try {
             Socket s = new Socket("localhost", 50000);
@@ -46,13 +40,13 @@ public class MyClient {
             System.out.println("Sent HELO to the server");
             bout.flush();
 
-            String serverReply = csj.readMsg(new byte[32], bin);
+            String serverReply = csj.readMsg(new byte[2], bin);
             System.out.println("Received in response to HELO: " + serverReply);
 
             bout.write(AUTH.getBytes());
             bout.flush();
 
-            serverReply = csj.readMsg(new byte[32], bin);
+            serverReply = csj.readMsg(new byte[2], bin);
             System.out.println("Received in response to AUTH: " + serverReply);
 
             bout.write(REDY.getBytes());
@@ -60,96 +54,93 @@ public class MyClient {
 
             while(jobsLeft) {
             
-            serverReply = csj.readMsg(new byte[32], bin);
-            System.out.println("Received in response to REDY: " + serverReply);
+                serverReply = csj.readMsg(new byte[64], bin);
+                System.out.println("Received in response to REDY: " + serverReply);
 
-            if(serverReply.substring(0,4).equals("NONE") || serverReply.substring(0,4).equals(QUIT)) {
-                jobsLeft = false;
-                bout.write(QUIT.getBytes());
-                bout.flush();
-                break;
-            }
-
-            if(!(serverReply.substring(0,4).equals("JOBN"))) {
-                bout.write("OK".getBytes());
-                bout.flush();
-                serverReply = csj.readMsg(new byte[124*124], bin);
-            System.out.println("Received in response to OK: " +serverReply);
-                bout.write(REDY.getBytes());
-                bout.flush();
-                
-            continue;
-            }
-
-            else if(serverReply.substring(0, 4).equals("JOBN")) {
-                String[] JOBNSplit = serverReply.split("\\s+");
-                int JobID = Integer.parseInt(JOBNSplit[2]);
-
-            bout.write("GETS All".getBytes());
-            bout.flush();
-
-            serverReply = csj.readMsg(new byte[32], bin);
-            System.out.println("Received in response to GETS All: " + serverReply);
-
-            
-
-            bout.write("OK".getBytes());
-            bout.flush();
-
-            String[] message_space = serverReply.split(" ");
-            if(message_space.length > 1) {
-                
-            serverReply = csj.readMsg(new byte[Integer.parseInt(message_space[1])*124], bin);
-    
-            }
-            else {
-                serverReply = csj.readMsg(new byte[10], bin);
-                
-            }
-            
-            
-            String[] arrOfStr = serverReply.split("\n");
-            if(largestServer == null){
-                String biggestServer = arrOfStr[0];
-
-                for(int i = 0; i < arrOfStr.length; i++) {
-                    String[] ServerSplitWord = arrOfStr[i].split("\\s+");
-                    String[] BigSplitWord = biggestServer.split("\\s+");
-                    int currentCore = Integer.parseInt(ServerSplitWord[4]);
-                    int bestCore = Integer.parseInt(BigSplitWord[4]);
-                    if(currentCore > bestCore) {
-                        biggestServer = arrOfStr[i];
-                    }
+                if(serverReply.substring(0,4).equals("NONE") || serverReply.substring(0,4).equals(QUIT)) {
+                    jobsLeft = false;
+                    bout.write(QUIT.getBytes());
+                    bout.flush();
+                    break;
                 }
-                String[] bigSplit = biggestServer.split("\\s+");
-                largestServer = bigSplit[0] + " " + bigSplit[1];
+
+                if(!(serverReply.substring(0,4).equals("JOBN"))) {
+                    bout.write(REDY.getBytes());
+                    bout.flush();
+                    continue;
+                }
+
+                else if(serverReply.substring(0, 4).equals("JOBN")) {
+                    String[] JOBNSplit = serverReply.split("\\s+");
+                    int JobID = Integer.parseInt(JOBNSplit[2]);
+
+                    bout.write("GETS All".getBytes());
+                    bout.flush();
+
+                    serverReply = csj.readMsg(new byte[32], bin);
+                    System.out.println("Received in response to GETS All: " + serverReply);
+
+                    bout.write("OK".getBytes());
+                    bout.flush();
+
+                    String[] message_space = serverReply.split(" ");
+
+                    String str = new String();
+                    for(int i = 0; i < message_space[2].length(); i++) {
+                        char c = message_space[2].charAt(i);
+                        if(c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' ||c == '6' || c == '7' || c == '8' || c == '9')
+                            str += c;
+                        else
+                            break;
+                    }
+                    
+                    serverReply = csj.readMsg(new byte[Integer.parseInt(message_space[1])*Integer.parseInt(str)], bin);
+                    
+                    //System.out.println(serverReply);
+
+                    String[] arrOfStr = serverReply.split("\n");
+                    if(largestServer == null){
+                        String biggestServer = arrOfStr[0];
+
+                        for(int i = 0; i < arrOfStr.length; i++) {
+                            String[] ServerSplitWord = arrOfStr[i].split("\\s+");
+                            String[] BigSplitWord = biggestServer.split("\\s+");
+                            int currentCore = Integer.parseInt(ServerSplitWord[4]);
+                            int bestCore = Integer.parseInt(BigSplitWord[4]);
+                            if(currentCore > bestCore) {
+                                biggestServer = arrOfStr[i];
+                            }
+                        }
+                        String[] bigSplit = biggestServer.split("\\s+");
+                        largestServer = bigSplit[0];
+                    }
+                    
+                    bout.write("OK".getBytes());
+                    bout.flush();
+
+                    serverReply = csj.readMsg(new  byte[1], bin);
+                    System.out.println("Received in response to OK is a dot: " + serverReply);
+
+                    String SCHD = "SCHD" + " " + JobID + " " + largestServer + " " + "0";
+                    bout.write(SCHD.getBytes());
+                    bout.flush();
+                    System.out.println("The biggest server is: " + largestServer);
+                    serverReply = csj.readMsg(new byte[2], bin);
+                    System.out.println("Received in response to SCHD: " + serverReply);
+                    
+                    bout.write(REDY.getBytes());
+                    bout.flush();
+
+                }
             }
-            
-            bout.write("OK".getBytes());
-            bout.flush();
-
-            serverReply = csj.readMsg(new  byte[1], bin);
-            System.out.println("Received in response to OK is a dot: " + serverReply);
-
-
-            String SCHD = "SCHD " + JobID + " " + largestServer + " 0";
-            bout.write(SCHD.getBytes());
-            bout.flush();
-            System.out.println("The biggest server is: " + largestServer);
-            serverReply = csj.readMsg(new byte[100], bin);
-            System.out.println("Received in response to SCHD: " + serverReply);
-            
-            bout.write(REDY.getBytes());
-            bout.flush();
-
-        }
-    }
             serverReply = csj.readMsg(new byte[32], bin);
             System.out.println("Received in response to QUIT: " + serverReply);
 
             if(serverReply.equals(QUIT)) {
                 bout.close();
                 dout.close();
+                bin.close();
+                din.close();
                 s.close();
             }
         }catch(Exception e) {
